f0803510c9742d4bd78dc19a030f5964a4509d56
samratluitel <ngc2058@gmail.com>
2018-05-26 09:26:56 +0300

    Mobile navigation finished

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 function OpenSlideMenu(){
     document.getElementById("side-menu").style.height="100vh";
-    document.getElementById("side-menu").style.width="250px";
+    document.getElementById("side-menu").style.width="200px";
+    document.getElementById("side-menu").style.padding="10px";
 }
 
 function CloseSlideMenu(){
-    setTimeout(()=>document.getElementById("side-menu").style.height="0px",300)
+    setTimeout(()=>{
+        document.getElementById("side-menu").style.height="0px";
+        document.getElementById("side-menu").style.padding="0px";
+    },300)
     document.getElementById("side-menu").style.width="0px";
 }
-
 
 window.FontAwesomeConfig = {
     searchPseudoElements: true
 }
+
+/*Side-Navigation Mobile Area */
+const scrollDowns= document.querySelectorAll("[data-scrolldown]");
+
+scrollDowns.forEach((scrollDown,index)=>{
+    const scrollDownUl=document.querySelector(`[data-scrolldownUl="${index}"]`);
+    scrollDown.addEventListener('click',()=>{
+        console.log(scrollDownUl)
+        scrollDownUl.classList.toggle('hide');
+    })
+})
 
 
 // Slider Area slider
